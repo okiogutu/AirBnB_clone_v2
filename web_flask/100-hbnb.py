dfc352ea88df03d1fa59e3a29e022dedd087c1c9
root <okiogutu@gmail.com>
2023-09-27 18:36:38 +0000

    questionable

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """Shows the main HBnB filters HTML page."""
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    return render_template("10-hbnb_filters.html",
+                           states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+"""starts a flask web application"""
+
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters')
+def hbnb_filters():
+    """HBnB filters HTML page"""
+    path = '100-hbnb.html'
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    places = storage.all(Place)
+    return render_template(path, states=states,
+                           amenities=amenities,
+                           places=places)
+
+
+@app.teardown_appcontext
+def app_teardown(arg=None):
+    """Removes current session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/5-number_template.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Returns a given string"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns a given string"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cText(text):
+    """display C"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythonText(text="is cool"):
+    """display Python"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def isNumber(n):
+    """display “n is a number” only if n is an integer"""
+    if isinstance(n, int):
+        return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n=None):
+    """display a HTML page only if n is an integer"""
+    if isinstance(n, int):
+        return render_template("5-number.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run()
+=======
+@app.route('/')
+def hello():
+    """Returns pecified string
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Returns specified string
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def cText(text):
+    """Return reformatted text
+    """
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_with_text(text='is cool'):
+    """Displays Python"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>')
+def number(n=None):
+    """Displays n if path variable is a valid integer
+    """
+    return str(n) + ' is a number'
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """Displays HTML text is n is a valid interger
+    """
+    path = '5-number.html'
+    return render_template(path, n=n)
+
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
